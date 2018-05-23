2cf12e1d651575434b37760b379360290c0fdb6c
Oliver <ot17409@bristol.ac.uk>
2018-05-23 09:10:54 +0100

    Added camfr install scripts

---
+++ installcamfrdeppt2.sh
@@ -0,0 +1,21 @@
+#sudo apt-get update
+#sudo apt-get install python-numpy python-scipy python-matplotlib python-pil* scons libboost* libblitz* libblas* liblapack*
+#git clone https://github.com/demisjohn/CAMFR.git
+cd CAMFR
+cp machine_cfg.py.gcc machine_cfg.py
+#open machine_cfg.py and change the python nubers to 2.7 and add (with no #)
+# include_dirs = ["/usr/include/python2.7", "/usr/lib/python2.7/numpy", "/usr/lib/python2.7/site-packa    ges"]
+echo 'include_dirs = ["/usr/include/python2.7", "/usr/lib/python2.7/numpy", "/usr/lib/python2.7/site-packages"]'
+gedit machine_cfg.py.gcc
+cd camfr
+#gedit camfr_wrap.cpp &
+#change the #include line to 
+#include "numpy/arrayobject.h"
+#
+echo '#include "numpy/arrayobject.h"' 
+gedit camfr_wrap.cpp 
+cd ../
+#sudo python setup.py install
+
+
+
---
+++ installcamfrdeppt3.sh
@@ -0,0 +1,6 @@
+cd CAMFR
+sudo python setup.py install
+cd visualisation
+sudo cp section_matplotlib.py /usr/local/lib/python2.7/dist-packages/camfr/section_matplotlib.py
+echo 'ALL DONE!!!!!!!'
+
